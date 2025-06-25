05fe6dda0139e0606cce677c7c1e40b640eb1c2a
Aditya Singh <adityaaa.singh27@gmail.com>
2025-06-25 20:19:11 +0530

    Css styling

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+export const Home = () => {
+
+    return (
+    <div className="min-h-screen ">
+        
+        {/* Theme Toggle */}
+
+        {/* BG effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+
+        {/* Footer */}
+    </div>
+    );
+};
